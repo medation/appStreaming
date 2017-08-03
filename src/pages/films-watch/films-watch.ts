d5f015c1da1b7a592b06a8b2c30ab8c38139825e
Elhachimi Med <Med@localhost.md>
2017-08-03 21:04:21 +0100

    last commit

--- src/pages/films-watch/films-watch.ts
+++ src/pages/films-watch/films-watch.ts
@@ -4,6 +4,8 @@ import { NavParams } from 'ionic-angular';
 
 import { DomSanitizer } from '@angular/platform-browser';
 
+import {SafeResourceUrl} from '@angular/platform-browser';
+
 @Component({
   selector: 'page-films-watch',
   templateUrl: 'films-watch.html'
@@ -11,9 +13,11 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class FilmsWatchPage {
   
   lecteur: any;
+  videoUrl: SafeResourceUrl;
 
-  constructor(public navParams: NavParams, public sanitizer: DomSanitizer) {
+  constructor(private domSanitizer: DomSanitizer, public navParams: NavParams, public sanitizer: DomSanitizer) {
     this.lecteur = navParams.data;
+    this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.lecteur.lien)
   }
 
   
